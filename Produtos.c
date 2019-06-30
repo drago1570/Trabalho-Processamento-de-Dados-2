19b170e20139c668c87a72c8884274972db3465f
drago1570 <39919568+drago1570@users.noreply.github.com>
2019-06-30 11:01:04 -0300

    Update Produtos.c

--- Produtos.c
+++ Produtos.c
@@ -1,7 +1,42 @@
-#include <stdio.h>
+//Este codigo não tem nada a ver com o proposito,
+//qua é levantar os dados sobre  o produto, apenas 
+//mostra na tela o que tem no arquivo.
 
-typedef struct Produtos{
+#include <stdio.h>
+#include <stdlib.h>
+#include "Produtos.h"
+#include "InterfaceUsuario.h"
+
+
+void price_mediun(){
 	
-	uint codigo,tipo;
-	float preco_atk,preco_var,quantidade;
-}produtos;
+  FILE *arq;
+  char Linha[100];
+  char *result;
+  int i;
+  system("clear");
+ 
+  arq = fopen(FILENAME, "rt");
+  if (arq == NULL) 
+   
+  {
+     printf("Problemas na abertura do arquivo\n");  
+  }
+  i = 1;
+  while (!feof(arq))
+  {
+	
+      result = fgets(Linha, 100, arq); 
+      if (result) 
+		printf("%s",Linha);
+      i++;
+  }
+  fclose(arq);
+}
+
+int main(){
+	
+	price_mediun();
+	
+}	 
+		
