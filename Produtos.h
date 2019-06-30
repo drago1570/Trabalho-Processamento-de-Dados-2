b3643f04c8b58cefaf498a23e1baa324f750848d
drago1570 <39919568+drago1570@users.noreply.github.com>
2019-06-30 11:02:16 -0300

    Update Produtos.h

--- Produtos.h
+++ Produtos.h
@@ -1,6 +1,14 @@
+//Aqui onde colocarei as funçoes do arquivo
+//produtos.c
+
 #ifndef _PRODUTOS_H_
 #define _PRODUTOS_H_
 
-struct produtos;
+typedef struct{
+	
+	int codigo,tipo;
+	float preco_atk,preco_var,quantidade;
+	
+}_Produtos;
 
 #endif
