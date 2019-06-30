a2b74c525e90d65b32f8f84f4002651f614e74cb
drago1570 <39919568+drago1570@users.noreply.github.com>
2019-06-30 10:58:22 -0300

    Update InterfaceUsuario.h

--- InterfaceUsuario.h
+++ InterfaceUsuario.h
@@ -4,5 +4,10 @@
 #include "InterfaceUsuario.c"
 
 void menu_principal();
+void Interface_cadastro();
+void Interfcae_remove();
+void print_mediunprice();
+void print_cheapprice();
+void print_statistcs();
 
 #endif
