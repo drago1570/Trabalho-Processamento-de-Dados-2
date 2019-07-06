311c251f7e701672546e62114db24a16306799cd
drago1570 <39919568+drago1570@users.noreply.github.com>
2019-07-06 01:26:58 -0300

    Update Arquivos.h

--- Arquivos.h
+++ Arquivos.h
@@ -3,8 +3,27 @@
 
 #include "Arquivos.c"
 
-void open_file(char *arq);
-void read_file(char *arq);
-void remove_products(char *arq);
+//Structs para a leitura dos dados dos arquivos txt
+
+typedef struct{
+	int	vet_cosmetico_codigo,vet_cosmetico_tipo;
+	float vet_cosmetico_prec_atk,vet_cosmetico_prec_var,vet_cosmetico_qntd;
+}_Vet_Cosmetico;
+
+typedef struct{
+	int	vet_higiene_codigo,vet_higiene_tipo;
+	float vet_higiene_prec_atk,vet_higiene_prec_var,vet_higiene_qntd;
+}_Vet_Higiene;
+
+typedef struct{
+	int	vet_cosmetico_codigo,vet_cosmetico_tipo;
+	float vet_cosmetico_prec_atk,vet_cosmetico_prec_var,vet_cosmetico_qntd;
+}_Vet_Alimento;
+
+//Procedimentos para manipulação de arquivos
+
+void show_file();
+FILE *arq(const char *arg, const char *mode);
 
 #endif
+
