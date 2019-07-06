f17e3fa4ce8ae9434fb4cb4bae3c8dc5d345b734
drago1570 <39919568+drago1570@users.noreply.github.com>
2019-07-06 01:27:38 -0300

    Update InterfaceUsuario.c

--- InterfaceUsuario.c
+++ InterfaceUsuario.c
@@ -2,27 +2,21 @@
 #include <stdlib.h>
 #include <locale.h>
 #include <string.h>
-#include "Arquivos.h"
 #include "Produtos.h"
+#include "Arquivos.h"
 #include <string.h>
-
-#define FILENAME "produtos.txt"
 
 void Interface_cadastro();
 void Interface_remove();
-void print_mediunprice();
-void print_cheapprice();
-void print_statistcs();	
 
-_Produtos Produtos;
-	
-_Produtos vetor[100];
 
 void menu_principal(){
 	
-	int escolha;
-	
+	_Choice choice;
+		
 	setlocale(LC_ALL, "Portuguese");
+	
+	do{
 	
 	printf("\t\t\t**************************************\n");
 	printf("\t\t\t*Sistema de Cadastramento de produtos*\n");
@@ -33,11 +27,13 @@ void menu_principal(){
 	printf("[3]Preço médio dos produtos\n");
 	printf("[4]Produtos mais baratos\n");
 	printf("[5]Dados estatisticos\n");
+	printf("[6]Mostrar Produtos\n");
+	printf("[7]Limpar Cadastros\n");
 	printf("[0]Sair\n\n");
 	printf("[*]Escolha uma opção: ");
-	scanf("%d",&escolha);
+	scanf("%d",&choice.escolha);
 	
-	switch(escolha){
+	switch(choice.escolha){
 	
 		case 1:
 			system("clear");	
@@ -45,37 +41,49 @@ void menu_principal(){
 			break;
 	
 		case 2:
-			Interface_remove();
+			system("clear");
 			break;
 	
 		case 3:
-			print_mediunprice();
+			system("clear");
 			break;
 	
 		case 4:
-			print_cheapprice();
+			system("clear");
 			break;
 	
 		case 5:
-			print_statistcs();
+			system("clear");
 			break;
 			
-		default:
-			printf("Comando Invalido!");
-			exit(EXIT_FAILURE);
+		case 6:
+			show_file();
+			break;
 			
+		case 7:{
+			
+			//FILE *file,*file_cosmeticos,*file_higiene,*file_alimentos;
+			clean_file("produtos.txt");
+			clean_file("cosmeticos.txt");
+			clean_file("higiene.txt");
+			clean_file("alimentos.txt");
+			break;
 		}
+		
+	}
+	}while(choice.escolha==0);
 }
 
 void Interface_cadastro(){
 	
 	int i,a;
 	
-	system("clear");
-
-	FILE *file;
+	FILE *file = arq("produtos.txt", "a");
+	FILE *file_cosmeticos = arq("cosmeticos.txt", "a");
+	FILE *file_higiene = arq("higiene.txt", "a");
+	FILE *file_alimentos = arq("alimentos.txt", "a");
 	
-	file = fopen(FILENAME,"w");
+	system("clear");
 	
 	_Produtos Produtos;
 	
@@ -84,6 +92,8 @@ void Interface_cadastro(){
 	for(i=0;i<100;i++){
 		
 		printf("\t\t\tCADASTRO\n\n\n");
+		
+		printf("\nO codigo contem 5 numeros!\n\n");
 		
 		printf("Código do produto: ");
 		scanf("%d",&Produtos.codigo);
@@ -96,9 +106,28 @@ void Interface_cadastro(){
 		printf("Quantidade disponivél no estoque: ");
 		scanf("%f",&Produtos.quantidade);
 		vetor[i] = Produtos;
+		
+		if(vetor[i].tipo==1){
+			fprintf(file_cosmeticos,"%d	%.2f	%.2f	%d	%.2f\n",
+			vetor[i].codigo,vetor[i].preco_atk,vetor[i].preco_var,vetor[i].tipo,vetor[i].quantidade);
+		}
+			
+		if(vetor[i].tipo==2){	
+			fprintf(file_higiene,"%d	%.2f	%.2f	%d	%.2f\n",
+			vetor[i].codigo,vetor[i].preco_atk,vetor[i].preco_var,vetor[i].tipo,vetor[i].quantidade);
+		}
+		
+		if(vetor[i].tipo==3){
+			fprintf(file_alimentos,"%d	%.2f	%.2f	%d	%.2f\n",
+			vetor[i].codigo,vetor[i].preco_atk,vetor[i].preco_var,vetor[i].tipo,vetor[i].quantidade);
+		}
 	
-		fprintf(file,"%d	|	%.2f	|	%.2f	|%d|	%.2f\n",vetor[i].codigo,
-		vetor[i].preco_atk,vetor[i].preco_var,vetor[i].tipo,vetor[i].quantidade);
+		fprintf(file,"Codigo: %d\n",vetor[i].codigo);
+		fprintf(file,"Preço atacado: %.2f\n",vetor[i].preco_atk);
+		fprintf(file,"Preço Varejo: %.2f\n",vetor[i].preco_var);
+		fprintf(file,"Tipo do produto (1-Cosmético; 2-Higiene; 3-Alimento): %d\n",vetor[i].tipo);
+		fprintf(file,"Quantidade: %d\n",vetor[i].tipo);
+		fprintf(file,"\n-----------------------------------------------------------\n");
 		
 		printf("\nVoce deseja adicionar mais produtos?(1-S/2-N): ");
 
@@ -111,9 +140,13 @@ void Interface_cadastro(){
 			
 		if(a==2){
 			break;
-
+			printf("Produtos cadastrados com sucesso!");
 		}
 	}
+	
+	if(fclose(file)==0 && fclose(file_cosmeticos)==0 && fclose(file_higiene)==0 && fclose(file_alimentos)==0){
+		printf("\nProdutos salvos com sucesso!");
+		}
 }
 
 void Interface_remove(){
@@ -138,17 +171,4 @@ void Interface_remove(){
 	
 	printf("Deseja remover mais produtos? (1-S/2-N): ");
 	}
-}
-
-
-
-void print_mediunprice(){
-	
-	//printf("Preço medio dos produtos: %.2f");
-}
-
-void print_cheapprice(){
-}
-
-void print_statistcs(){
 }
