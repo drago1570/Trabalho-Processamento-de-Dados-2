6970636ed536e619fd323c60ec7c36efc83e3d2f
drago1570 <39919568+drago1570@users.noreply.github.com>
2019-07-07 00:30:22 -0300

    Update Produtos.h

--- Produtos.h
+++ Produtos.h
@@ -1,6 +1,8 @@
 #ifndef _PRODUTOS_H_
 #define _PRODUTOS_H_
 
+#include "Produtos.c"
+
 typedef struct{
 	
 	int codigo,tipo;
@@ -12,5 +14,9 @@ typedef struct{
 	
 	int escolha;
 }_Choice;
+
+void mediun_price(const char *arq);
+void cheap_price_atacado(const char *arq);
+void cheap_price_varejo(const char *arq);
 
 #endif
